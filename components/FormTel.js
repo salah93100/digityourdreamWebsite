cf8dc78a7974b68fea728a8686827d029106a201
Salah <salah.elbouhali@digityourdream.fr>
2022-11-05 00:04:39 +0100

    fix error importation

--- components/FormTel.js
+++ components/FormTel.js
@@ -1,9 +1,8 @@
 import {React,useState,useEffect} from 'react'
 import PhoneInput from "react-phone-input-2"
 import "react-phone-input-2/lib/material.css"
-import { BiPhoneCall } from '@react-icons/all-files/Bi/BiPhoneCall';
+import { BiPhoneCall } from '@react-icons/all-files/bi/BiPhoneCall';
 import { AiOutlineCheckCircle } from '@react-icons/all-files/ai/AiOutlineCheckCircle';
-
 
 
 
