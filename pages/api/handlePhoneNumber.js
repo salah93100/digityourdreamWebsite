a739c4b1b6e9fcb49d9620d7065fb5af25b9e4c8
Salah <salah.elbouhali@digityourdream.fr>
2023-02-13 16:39:07 +0100

    add .env and sup formtet bug

--- pages/api/handlePhoneNumber.js
+++ pages/api/handlePhoneNumber.js
@@ -5,8 +5,6 @@ export default async function handlePhoneNumber(req, res) {
     const {phoneNumberData}=  JSON.parse(req.body);
     const data = await sanityClient
     .patch('drafts.af5cae99-52a2-43a1-8b62-b681151ecffc')
-    // Ensure that the `reviews` arrays exists before attempting to add items to it
-    .setIfMissing({numberphone: ["ddd"]})
     // Add the items after the last item in the array (append)
     .insert('after', 'numberphone[-1]', [phoneNumberData])
     .commit({
@@ -15,7 +13,6 @@ export default async function handlePhoneNumber(req, res) {
       autoGenerateArrayKeys: true,
     })
     .catch((error)=> console.log(error))
-    console.log(phoneNumberData)
     res.status(200).json({data})
   }
   
